b07676dc4e70ab535f120b7eb716af74433a5f35
Rahman Yusuf <mansuf@mansuf.link>
2024-05-21 12:42:21 +0700

    ensure we can import firebase key from a file

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import os
 from app import main
+from pathlib import Path
 
-if os.environ.get("FIREBASE_API_KEY") is None:
+firebase_file_key = "firebase-web-api.key"
+
+if os.path.exists(firebase_file_key):
+    os.environ.setdefault("FIREBASE_API_KEY", Path(firebase_file_key).read_text())
+
+elif os.environ.get("FIREBASE_API_KEY") is None:
     api_key = input("Masukkan firebase api key anda\n=> ")
 
-    os.environ.setdefault("FIREBASE_API_KEY")
+    os.environ.setdefault("FIREBASE_API_KEY", api_key)
 
 main()
