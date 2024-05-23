4fbbd2130bd4b11afaacf40adaf1a5641a3edbc1
Rahman Yusuf <mansuf@mansuf.link>
2024-05-23 17:53:25 +0700

    change method as requested

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -1,6 +1,6 @@
 from .views import register_user, login
 
 urls_patterns = [
-    {"rule": "/auth/register", "view_func": register_user, "methods": ["POST"]},
-    {"rule": "/auth/login", "view_func": login, "methods": ["POST"]},
+    {"rule": "/auth/register", "view_func": register_user, "methods": ["GET"]},
+    {"rule": "/auth/login", "view_func": login, "methods": ["GET"]},
 ]
