af4054f534b849ee466ddbaded8d00da042b9358
Rahman Yusuf <mansuf@mansuf.link>
2024-06-01 16:12:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 # Stop app container
-docker ps --filter status=running --filter name=cc-api-main -q | xargs docker stop
+sudo docker ps --filter status=running --filter name=cc-api-main -q | xargs docker stop
 
 # Delete app container
-docker ps --filter status=exited --filter name=cc-api-main -q | xargs docker rm
+sudo docker ps --filter status=exited --filter name=cc-api-main -q | xargs docker rm
 
 # Delete app image
-docker image rm cc-api --force
+sudo docker image rm cc-api --force
 
 # Build image
-docker build -t cc-api:0.0.1 .
+sudo docker build -t cc-api:0.0.1 .
 
 # Start it up
-docker run -p "127.0.0.1:5000:5000" --name "cc-api-main" -d cc-api:0.0.1
+sudo docker run -p "127.0.0.1:5000:5000" --name "cc-api-main" -d cc-api:0.0.1
