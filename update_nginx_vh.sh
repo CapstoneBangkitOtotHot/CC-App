b5ac9d26a76438f7e7a41a7e4f274e2ccdb42903
Rahman Yusuf <mansuf@mansuf.link>
2024-06-02 15:51:57 +0700

    fix script to update nginx host

--- update_nginx_vh.sh
+++ update_nginx_vh.sh
@@ -1,4 +1,4 @@
-sudo cp ./*.host /etc/nginx/sites-available
-sudo cp ./*.host /etc/nginx/sites-enabled
+sudo cp ./*.nginx.host /etc/nginx/sites-available
+sudo cp ./*.nginx.host /etc/nginx/sites-enabled
 sudo nginx -t
 sudo systemctl reload nginx
