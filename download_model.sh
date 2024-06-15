b7f7a79767f1afefe0795d630e7d57d4556df45f
Reylyer <reylyercross@gmail.com>
2024-06-16 03:18:08 +0700

    fix destination download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,4 +1,4 @@
-gsutil cp gs://ai-models-c241-ps005/apple.keras /local/path/to/save/apple.keras app/machine_learning_backend/ML_Backend/models/apple.keras
+gsutil cp gs://ai-models-c241-ps005/apple.keras app/machine_learning_backend/ML_Backend/models/apple.keras
 gsutil cp gs://ai-models-c241-ps005/classification_model.pt app/machine_learning_backend/ML_Backend/models/classification_model.pt
 gsutil cp gs://ai-models-c241-ps005/mango.keras app/machine_learning_backend/ML_Backend/models/mango.keras
 gsutil cp gs://ai-models-c241-ps005/sapodilla.keras app/machine_learning_backend/ML_Backend/models/sapodilla.keras
