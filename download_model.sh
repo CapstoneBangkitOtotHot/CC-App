34a484eee0ccb5f46c746bb8947eed0e5389dc06
Rahman Yusuf <mansuf@mansuf.link>
2024-06-16 15:17:31 +0700

    fix download model

--- download_model.sh
+++ download_model.sh
@@ -1,5 +1,5 @@
-wget https://storage.googleapis.com/ai-models-c241-ps005/apple.keras
-wget https://storage.googleapis.com/ai-models-c241-ps005/classification_model.pt
-wget https://storage.googleapis.com/ai-models-c241-ps005/mango.keras
-wget https://storage.googleapis.com/ai-models-c241-ps005/sapodilla.keras
-wget https://storage.googleapis.com/ai-models-c241-ps005/tomato.keras
+wget -P app/machine_learning_backend/ML_Backend/models https://storage.googleapis.com/ai-models-c241-ps005/apple.keras
+wget -P app/machine_learning_backend/ML_Backend/models https://storage.googleapis.com/ai-models-c241-ps005/classification_model.pt
+wget -P app/machine_learning_backend/ML_Backend/models https://storage.googleapis.com/ai-models-c241-ps005/mango.keras
+wget -P app/machine_learning_backend/ML_Backend/models https://storage.googleapis.com/ai-models-c241-ps005/sapodilla.keras
+wget -P app/machine_learning_backend/ML_Backend/models https://storage.googleapis.com/ai-models-c241-ps005/tomato.keras
